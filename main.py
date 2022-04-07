02664dde6b4466838a9ce4708270b5a33f3c1bb9
Bambinexx <72572215+Bambinexx@users.noreply.github.com>
2022-04-07 23:30:42 +0200

    tryna get song playing to work
    I get a weird error when I try to play any music, I'll focus on fixing that tmrw

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from time import sleep
 from os import listdir
 from itertools import *
-from pygame import mixer
+from vlc import *
 
 class Reader:
 
@@ -37,30 +37,19 @@ class Reader:
 
     def osu_reader(self):
 
-        mainframe = ttk.Frame(self.reader, padding="2 2")
-        mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
+        self.mainframe = ttk.Frame(self.reader, padding="2 2")
+        self.mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
 
         if self.osufolder.get() == "None":
-            popup_gen = ttk.Label(mainframe, text="You haven't defined a folder yet! Click the button below to choose it!")
+            popup_gen = ttk.Label(self.mainframe, text="You haven't defined a folder yet! Click the button below to choose it!")
             popup_gen.grid(column=1, row=1)
-            ttk.Button(mainframe, text="clicc", command=self.not_defined_folder).grid(column=1, row=2)
-
-        else:
-            arr = listdir(self.osufolder.get() + "/Songs/")
-            music_selection_menu = ttk.Frame(mainframe, padding=f"1 {len(arr)}").grid(column=2, row=2)
-            scrollbar = ttk.Scrollbar(music_selection_menu, orient="vertical")
-            scrollbar.grid(column=1)
-
-            for i,j in enumerate(arr):
-
-                music = j.split("-")
-
-                ttk.Label(music_selection_menu, text=music).grid(column=0, row=i+1, sticky=(W))
-        
+            button = ttk.Button(self.mainframe, text="clicc", command= lambda: self.not_defined_folder(popup_gen, button))
+            button.grid(column=1, row=2)
+            
         self.reader.mainloop()
 
 
-    def not_defined_folder(self):
+    def not_defined_folder(self, *args):
 
         self.popup = Toplevel()
         self.popup.title("Folder location")
@@ -70,12 +59,45 @@ class Reader:
         ttk.Label(mainframe, text="Choose your osu! folder location").grid(column=1, row=1)
         ttk.Button(mainframe, text="clicc", command=self.askdirectory).grid(column=1, row=2)
         ttk.Label(mainframe, textvariable=self.osufolder).grid(column=1, row=3, sticky=(W, E))
-        ttk.Button(mainframe, text="OK", command=self.popup.destroy).grid(column=1, row=4)
+        ttk.Button(mainframe, text="OK", command= lambda: [self.update_list(args), self.popup.destroy()]).grid(column=1, row=4)
         self.popup.mainloop()
 
 
     def askdirectory(self):
 
         self.osufolder.set(filedialog.askdirectory())
+    
+
+    def update_list(self, *args):
+        
+        for i in args[0]:
+            i.destroy()
+
+        self.arr = listdir(self.osufolder.get() + "/Songs/")
+        self.music_selection_menu = ttk.Frame(self.mainframe, padding=f"1 {len(self.arr)}").grid(column=2, row=2)
+        scrollbar = ttk.Scrollbar(self.music_selection_menu, orient="vertical")
+        scrollbar.grid(column=1)
+
+        for i,j in enumerate(self.arr):
+
+            self.arr[i] = self.osufolder.get() + "/Songs/" + j
+
+            ttk.Button(self.music_selection_menu, text=self.arr[i], command= lambda: self.play_music(self.arr[i])).grid(column=0, row=i+1, sticky=(W))
+        
+        print(self.arr)
+    
+
+    def play_music(self, path):
+
+        print(path)
+        for file in path:
+
+            if ".mp3" in file:
+                audio = file
+
+        p = MediaPlayer(file)
+        p.play()
+
+        p.stop()
 
 a = Reader()
