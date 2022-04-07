d4ef1f0527e083c49a2d42533c06812f66a9bd58
Bambinexx <72572215+Bambinexx@users.noreply.github.com>
2022-04-07 21:43:41 +0200

    brug
    idk man it just doesn't work lol

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ from pygame import mixer
 class Reader:
 
     def __init__(self) -> None:
-        
+
         self.reader = Tk()
         self.reader.title("osu! music reader")
-        self.reader.geometry(f"{self.reader.winfo_screenwidth()}x{self.reader.winfo_screenheight()}")
+
+        self.width = self.reader.winfo_screenwidth()
+        self.height = self.reader.winfo_screenheight()
+        self.reader.geometry(f"{self.width}x{self.height}")
 
         try:
             self.open_cfg()
@@ -20,39 +23,45 @@ class Reader:
         except FileNotFoundError:
             with open("config.txt", "x") as cfg:
                 cfg.write("folder: None")
-            
+
             self.open_cfg()
 
 
     def open_cfg(self):
         with open("config.txt", "r") as cfg:
+
             self.osufolder = StringVar()
             self.osufolder.set(cfg.readline().replace("folder: ", "").replace("\n", ""))
-
-            if self.osufolder.get() == "None":
-                self.not_defined_folder()
-        
             self.osu_reader()
 
 
     def osu_reader(self):
-        arr = listdir(self.osufolder.get() + "/Songs/")
+
         mainframe = ttk.Frame(self.reader, padding="2 2")
         mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
 
-        music_selection_menu = ttk.Frame(mainframe, padding=f"1 {len(arr)}").grid(column=2, row=2)
-        
-        for i,j in enumerate(arr):
-            
-            music = j.split("-")
-            print(music)
+        if self.osufolder.get() == "None":
+            popup_gen = ttk.Label(mainframe, text="You haven't defined a folder yet! Click the button below to choose it!")
+            popup_gen.grid(column=1, row=1)
+            ttk.Button(mainframe, text="clicc", command=self.not_defined_folder).grid(column=1, row=2)
 
-            """ttk.Label(music_selection_menu, text=artist + name).grid(column=1, row=i, sticky=(W))"""
+        else:
+            arr = listdir(self.osufolder.get() + "/Songs/")
+            music_selection_menu = ttk.Frame(mainframe, padding=f"1 {len(arr)}").grid(column=2, row=2)
+            scrollbar = ttk.Scrollbar(music_selection_menu, orient="vertical")
+            scrollbar.grid(column=1)
+
+            for i,j in enumerate(arr):
+
+                music = j.split("-")
+
+                ttk.Label(music_selection_menu, text=music).grid(column=0, row=i+1, sticky=(W))
         
         self.reader.mainloop()
 
 
     def not_defined_folder(self):
+
         self.popup = Toplevel()
         self.popup.title("Folder location")
         mainframe = ttk.Frame(self.popup, padding="1 4 12 12")
@@ -61,12 +70,12 @@ class Reader:
         ttk.Label(mainframe, text="Choose your osu! folder location").grid(column=1, row=1)
         ttk.Button(mainframe, text="clicc", command=self.askdirectory).grid(column=1, row=2)
         ttk.Label(mainframe, textvariable=self.osufolder).grid(column=1, row=3, sticky=(W, E))
-        ttk.Button(mainframe, text="OK", command=self.popup.quit).grid(column=1, row=4)
+        ttk.Button(mainframe, text="OK", command=self.popup.destroy).grid(column=1, row=4)
         self.popup.mainloop()
 
 
     def askdirectory(self):
+
         self.osufolder.set(filedialog.askdirectory())
-        self.popup.update()
 
 a = Reader()
